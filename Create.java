428630d8c44604b46f649bed807f96049c13206a
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-08-12 19:05:53 +0530

    Array

--- Create.java
+++ Create.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 public class Create{
 public static void main(String[]args){
 // Create an Object for ArrayList
-  // Load Values in ArrayList
   // Iterate it by usig Iterator and While Loops
+     
+  // Create an Object for ArrayList
+        ArrayList<String> a = new ArrayList<>();
+
+        // Load Values in ArrayList
+        a.add("Ritesh");
+        a.add("Ritu");
+        a.add("Bala");
+
+        // Iterate it using Iterator and While Loops
+        Iterator<String> iterator = a.iterator();
+        
+        // using while loop
+        while (iterator.hasNext()) {
+            String item = iterator.next();
+            System.out.println(item);
+        
+    }
 }
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,44 @@
-public class Dynamic{
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Load person class Objects in it
-  // Iterate it by using List Iterator
-  // print the values by using while Loop
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    // Override toString method for easy printing
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
 }
+
+public class Dynamic {
+    public static void main(String[] args) {
+        
+        ArrayList<Person> personList = new ArrayList<>();
+
+        
+        personList.add(new Person("Ritesh", 22));
+        personList.add(new Person("Balu", 25));
+       
+        // Iterate it by using ListIterator
+        ListIterator<Person> listIterator = personList.listIterator();
+        
+        // Print the values by using while Loop
+        while (listIterator.hasNext()) {
+            Person person = listIterator.next();
+            System.out.println(person);
+        }
+    }
 }
